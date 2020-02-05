0dc1c2d29525629949bfe26cfcbf56e19f65524c
Urosh Trifunovic <nediarovic@gmail.com>
2020-02-05 20:27:17 +0100

    Update formatting

--- mysql-automapping-demo/src/main/java/com/urosht/demo/entity/Cat.java
+++ mysql-automapping-demo/src/main/java/com/urosht/demo/entity/Cat.java
@@ -19,14 +19,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class Cat {
+
 	@Id
 	@Column(name = "id", columnDefinition = "BIGINT(20)")
 	private String name;
+
 	@Column(name = "age", columnDefinition = "INT(4)")
 	private int age;
+
 	@Column(name = "color", columnDefinition = "VARCHAR(20)")
 	private String color;
+
 	@Enumerated(EnumType.STRING)
-    @Column(name = "favorite_food")
-    private FavoriteFood favoriteFood;
+  @Column(name = "favorite_food")
+  private FavoriteFood favoriteFood;
 }
